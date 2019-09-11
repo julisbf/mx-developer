f27ed1840b4031bcc479c91bf410ebf32e353c67
Huang-Ming Chang <huang.m.chang@gmail.com>
2019-09-11 15:35:53 +0200

    Solve merge conflicts

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { getCurrentApp, BEAVER } from '../utils/environmentHelpers';
+import { getCurrentApp, BEAVER, DATAHUB } from '../utils/environmentHelpers';
 import navbarItemData from '../resources/menu/navbar.json';
 
 const NavBar = ({
@@ -11,18 +11,26 @@ const NavBar = ({
     children: Function;
 }) => {
     const currentApp = getCurrentApp();
-    /**
-     * Why the `filter`?
-     * We need to hide Sprintr-related NavBarItems on `beaver` (apps owned by digital ecosystem team)
-     *  */
+
     return (
         <div className={className}>
             {navbarItemData
-                .filter(
-                    itemData =>
+                .filter(itemData => {
+                    /**
+                     * hide Sprintr-related NavBarItems on `beaver` (apps owned by digital ecosystem team)
+                     *  */
+                    return (
                         currentApp !== BEAVER ||
                         (currentApp === BEAVER && !itemData.microflow)
-                )
+                    );
+                })
+                .filter(itemData => {
+                    /**
+                     * only show `Data Hub` when it's on `https://hub.mendixcloud.com`
+                     *  */
+
+                    return currentApp === DATAHUB || itemData.key !== DATAHUB;
+                })
                 .map(itemData => children(itemData))}
         </div>
     );
